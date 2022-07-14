ff5124411ac45a07d7803a7c66ca75893ddfc454
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-07-15 05:08:48 +0900

    update c project

---
+++ c/printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void)
+{
+	printf ("Hello, World.\n");
+	printf ("Hello, World.\n");
+	printf ("Hello, World. Kyungsung University.\n");
+	printf ("Kyungsung University.\n");
+	printf ("Kyungsung University. Dept. of Computer Science. \n");
+	printf ("Hello, World. Kyungsung University. Dept. of Computer Science.\n ");
+				
+return 0;
+}
