bc5d0484ad20176c5e551212fc835ede7199f07c
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-08-09 00:25:30 +0900

    update c project

---
+++ c/pro30.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main (void)
+{
+	char s[20];
+	int a;
+	scanf ("%s", &s);
+	a = strlen (s);
+	
+	for (int in=a; in == 0 ;in--)
+	{
+		printf ("%c\n", x[in-1]);			
+	}
+	return 0;
+}
+
+
+
+	for 
+	{
+		char x[a] = s;
+	}
